e64b5593387cef80df464a91e8ee8785eef436ea
vlad <vlad@DXBU-D9C2J42>
2015-08-26 13:05:33 +0300

    da

---
+++ testele-mele/testele-mele-http-server/src/main/java/ro/daephenceeva/httpd/BadParseException.java
@@ -0,0 +1,17 @@
+package ro.daephenceeva.httpd;
+
+import java.io.IOException;
+
+public class BadParseException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public BadParseException(IOException e) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadParseException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
